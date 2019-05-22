0be30580dd18a51e93a3ef43be1e2bc736759598
szwedoman <szwedomaster@gmail.com>
2019-05-22 18:24:22 +0200

    settings models and layout

---
+++ app/src/main/java/pl/wat/pks/recylerModels/Setting.java
@@ -0,0 +1,8 @@
+package pl.wat.pks.recylerModels;
+
+public class Setting {
+    private String currencyName;
+    private boolean notifficationBool;
+    private double exchangeRateMax;
+    private double exchangeRateMin;
+}
