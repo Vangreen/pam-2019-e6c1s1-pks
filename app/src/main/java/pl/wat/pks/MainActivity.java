c18aaa7d134ddfb6b5c0619dcb7d79c4a3a9d871
vangreen <pwangryn@op.pl>
2019-05-22 15:57:01 +0200

    Clean up

--- app/src/main/java/pl/wat/pks/MainActivity.java
+++ app/src/main/java/pl/wat/pks/MainActivity.java
@@ -3,7 +3,6 @@ package pl.wat.pks;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -11,10 +10,9 @@ import androidx.viewpager.widget.ViewPager;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
-public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener, Tab2.OnFragmentInteractionListener, Tab3.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements PortfelTab.OnFragmentInteractionListener, AktualneKursyTab.OnFragmentInteractionListener, UstawieniaTab.OnFragmentInteractionListener {
     ViewPager viewPager;
     AHBottomNavigation bottomNavigation;
-    TextView headertxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +23,20 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
         this.createNavigationItem();
 
         //pages
-
-
         viewPager = findViewById(R.id.pager);
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 3);
         viewPager.setAdapter(adapter);
-
     }
 
 
     private void initialViews() {
         bottomNavigation = findViewById(R.id.bottom_navigation);
-        headertxt = findViewById(R.id.headerLabel);
     }
 
     private void createNavigationItem() {
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.tab1, R.drawable.ic_wallet, R.color.design_default_color_primary);
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.tab2, R.drawable.ic_chart_white, R.color.design_default_color_primary);
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.tab3, R.drawable.ic_settings_white_24dp, R.color.design_default_color_primary);
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem(R.string.portfelTab, R.drawable.ic_wallet, R.color.design_default_color_primary);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem(R.string.aktualneKursyTab, R.drawable.ic_chart_white, R.color.design_default_color_primary);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem(R.string.ustawieniaTab, R.drawable.ic_settings_white_24dp, R.color.design_default_color_primary);
 
         bottomNavigation.addItem(item1);
         bottomNavigation.addItem(item2);
@@ -57,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
         //use colored navigation with animation :~D
         bottomNavigation.setColored(true);
 
-        //
+        //default tab
         bottomNavigation.setCurrentItem(0);
 
         bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
@@ -76,16 +70,12 @@ public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentIn
                     default:
                         break;
                 }
-
                 return true;
             }
         });
-
-
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-
     }
 }
