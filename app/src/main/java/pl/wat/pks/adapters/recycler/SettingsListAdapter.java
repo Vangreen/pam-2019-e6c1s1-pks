0822de4e5a88d0414ca651da97818b065f049bcb
szwedoman <szwedomaster@gmail.com>
2019-05-23 15:53:30 +0200

    Recycler view working only as display. Better Design

--- app/src/main/java/pl/wat/pks/adapters/recycler/SettingsListAdapter.java
+++ app/src/main/java/pl/wat/pks/adapters/recycler/SettingsListAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,13 +15,13 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import java.util.List;
 
 import pl.wat.pks.R;
-import pl.wat.pks.models.recyler.Setting;
+import pl.wat.pks.models.recycler.CurrencySettingModel;
 
 public class SettingsListAdapter extends RecyclerView.Adapter<SettingsListAdapter.ViewHolder> {
 
-    private List<Setting> settingsList;
+    private List<CurrencySettingModel> settingsList;
 
-    public SettingsListAdapter(List<Setting> settingsList) {this.settingsList = settingsList;}
+    public SettingsListAdapter(List<CurrencySettingModel> settingsList) {this.settingsList = settingsList;}
 
     @NonNull
     @Override
@@ -37,12 +38,15 @@ public class SettingsListAdapter extends RecyclerView.Adapter<SettingsListAdapte
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         //Holdery
+        String currencyName = settingsList.get(position).getCurrencyName();
+        holder.currencyName.setText(currencyName);
         holder.currencyIcon.setImageDrawable(settingsList.get(position).getCurrencyIcon());
         String exchangeRateMin = String.valueOf(settingsList.get(position).getExchangeRateMin());
         holder.currencyExRateMin.setText(exchangeRateMin);
         String exchangeRateMax = String.valueOf(settingsList.get(position).getExchangeRateMax());
         holder.currencyExRateMax.setText(exchangeRateMax);
-        holder.notificationSwitch.setChecked(settingsList.get(position).getNotificationBool());
+        boolean notificationBoll = settingsList.get(position).isNotificationBool();
+        holder.notificationSwitch.setChecked(notificationBoll);
 
     }
 
@@ -57,11 +61,13 @@ public class SettingsListAdapter extends RecyclerView.Adapter<SettingsListAdapte
         public SwitchMaterial notificationSwitch;
         public EditText currencyExRateMax;
         public EditText currencyExRateMin;
+        public TextView currencyName;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
             currencyIcon = itemView.findViewById(R.id.currencyIcon);
+            currencyName = itemView.findViewById(R.id.currencyNameTextView);
             notificationSwitch = itemView.findViewById(R.id.notificationSwitch);
             currencyExRateMax = itemView.findViewById(R.id.currencyExRateMax);
             currencyExRateMin = itemView.findViewById(R.id.currencyExRateMin);
