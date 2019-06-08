de733fb6a84851dd159b769c7ba5b870bd15e714
szwedoman <szwedomaster@gmail.com>
2019-06-08 19:04:40 +0200

    CurrencySettingDao

---
+++ app/src/main/java/pl/wat/pks/currency/settings/CurrencySettingDao.java
@@ -0,0 +1,25 @@
+package pl.wat.pks.currency.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface CurrencySettingDao {
+    @Insert
+    void insert(CurrencySetting word);
+
+    @Delete
+    void delete(CurrencySetting word);
+
+    @Update
+    void update(CurrencySetting word);
+
+    @Query("SELECT * from CurrencySetting")
+    LiveData<List<CurrencySetting>> getAllCurrencySettings();
+}
