b9648654dec898053f3b65ebdfe5500b132000c6
Sebastian Strzalkowski <sebastianstrzalkowski.strzala@gmail.com>
2019-06-11 11:26:24 +0200

    Clean ActualCourseView

--- app/src/main/java/pl/wat/pks/views/ActualCourseView.java
+++ app/src/main/java/pl/wat/pks/views/ActualCourseView.java
@@ -21,17 +21,8 @@ import pl.wat.pks.models.dto.XYCorordinates;
 import pl.wat.pks.rest.RestController;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ActualCourseView.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ActualCourseView#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ActualCourseView extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -50,7 +41,6 @@ public class ActualCourseView extends Fragment {
     private ValueLineChart mCubicValueLineChart;
 
     public ActualCourseView() {
-        // Required empty public constructor
     }
 
     /**
@@ -154,7 +144,6 @@ public class ActualCourseView extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.aktualne_kursy_fragment, container, false);
 
         mCubicValueLineChart = rootView.findViewById(R.id.cubiclinechart);
@@ -162,12 +151,6 @@ public class ActualCourseView extends Fragment {
         return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
@@ -186,18 +169,8 @@ public class ActualCourseView extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
